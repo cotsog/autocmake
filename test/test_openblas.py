6a5f8ba65b14948333a840461c152284f3e45a62
Miro ILIAS <Miroslav.Ilias@umb.sk>
2016-06-01 10:28:14 +0200

    - for openblas testing, uninstall previous math libs packages   (blas,lapack,atlas) and install openblas packages and run own test

--- test/test_openblas.py
+++ test/test_openblas.py
@@ -83,68 +83,10 @@ def test_extra_cmake_options():
     configure_build_and_exe('extra_cmake_options', 'python setup --cxx=g++ --cmake-options="-DENABLE_SOMETHING=OFF -DENABLE_FOO=ON"')
 
 
-def test_cxx():
-    configure_build_and_exe('cxx', 'python setup --cxx=g++')
-
-
 def test_fc():
     configure_build_and_exe('fc', 'python setup --fc=gfortran')
 
 
-def test_fc_git_info():
-    configure_build_and_exe('fc_git_info', 'python setup --fc=gfortran')
-
-
-def test_fc_int64():
-    configure_build_and_exe('fc_int64', 'python setup --fc=gfortran --int64')
-
-
 @skip_on_osx
-def test_fc_omp():
-    os.environ['OMP_NUM_THREADS'] = '2'
-    configure_build_and_exe('fc_omp', 'python setup --omp --fc=gfortran')
-
-
-def test_fc_blas():
-    configure_build_and_exe('fc_blas', 'python setup --fc=gfortran --blas')
-
-
-def test_fc_lapack():
-    configure_build_and_exe('fc_lapack', 'python setup --fc=gfortran --lapack')
-
-
-@skip_on_osx
-def test_cxx_cblas():
-    configure_build_and_exe('cxx_cblas', 'python setup --cxx=g++ --cblas')
-
-
-@skip_on_linux
-def test_cxx_accelerate():
-    configure_build_and_exe('cxx_accelerate', 'python setup --cxx=g++ --accelerate')
-
-
-def test_python_interpreter():
-    configure_build_and_exe('python_interpreter', 'python setup --cxx=g++')
-
-
-def test_python_interpreter_custom():
-    setup = 'python setup --cxx=g++ --python=%s' % sys.executable
-    configure_build_and_exe('python_interpreter_custom', setup)
-
-
-def test_python_libs():
-    configure_build_and_exe('python_libs', 'python setup --cxx=g++')
-
-
-def test_python_libs_custom():
-    python_executable = sys.executable
-    configure_build_and_exe('python_libs_custom', 'python setup --cxx=g++ --python={0}'.format(python_executable))
-
-
-def test_boost_header_only():
-    configure_build_and_exe('boost_header_only', 'python setup --cxx=g++')
-
-
-@skip_on_osx
-def test_boost_libs():
-    configure_build_and_exe('boost_libs', 'python setup --cxx=g++ --mpi')
+def test_fc_openblas():
+    configure_build_and_exe('fc_openblas', 'python setup --fc=gfortran --openblas')
